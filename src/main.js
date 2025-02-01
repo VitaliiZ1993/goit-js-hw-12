9ef4f56030fcec14ee527735741f4e31e1563eba
Vitalii Zaidenshnir <sapsan281993@gmail.com>
2025-02-01 23:23:31 +0100

    modern

--- src/main.js
+++ src/main.js
@@ -111,3 +111,20 @@ function scrollBar() {
     });
   }
 }
+
+const backToTopBtn = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.style.display = 'block'; 
+  } else {
+    backToTopBtn.style.display = 'none'; 
+  }
+});
+
+backToTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
